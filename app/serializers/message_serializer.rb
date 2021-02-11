5fffeea617066919d6841248a4e34ef934789872
Ramona Maltan <ramonamaltan@gmail.com>
2021-02-11 15:53:10 +0100

    added jsonapi serializer cache option

--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -3,4 +3,6 @@ class MessageSerializer
   attributes :content, :counter
   has_many :links
   has_many :emails
+
+  cache_options store: Rails.cache, namespace: 'jsonapiserializer', expires_in: 1.hour
 end
