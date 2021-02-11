a29803ab2139ec4785641897872ec9323e55a1f9
Ramona Maltan <ramonamaltan@gmail.com>
2021-02-11 14:17:08 +0100

    added counter to messages table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_11_101634) do
+ActiveRecord::Schema.define(version: 2021_02_11_131614) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2021_02_11_101634) do
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "counter"
   end
 
   add_foreign_key "emails", "messages"
